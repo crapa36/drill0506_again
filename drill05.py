ffdd33e98927060d29b5ea69d4477c35e38bdf9d
crapa-Desktop <crapa36@gmail.com>
2023-10-04 22:24:36 +0900

    반복적으로 만들어지는 랜덤 손 화살표 위치로 캐릭터 이동 구현 완료. t 값이 1을 넘으면, 다시 새로운 손 화살표가 랜덤위치에 만들어짐

--- drill05.py
+++ drill05.py
@@ -1,13 +1,14 @@
 from pico2d import *
 import random
+
 TUK_WIDTH, TUK_HEIGHT = 1280, 1024
 
 
 def load_resources():
     global TUK_ground, character, arrow
-    arrow = load_image('hand_arrow.png')
-    TUK_ground = load_image('TUK_GROUND.png')
-    character = load_image('animation_sheet.png')
+    arrow = load_image("hand_arrow.png")
+    TUK_ground = load_image("TUK_GROUND.png")
+    character = load_image("animation_sheet.png")
 
 
 def handle_events():
@@ -22,33 +23,41 @@ def handle_events():
 
 
 def reset_world():
-    global running, cx, cy, frame, hx, hy,sx,sy, t, action
+    global running, cx, cy, frame, hx, hy, sx, sy, t, action
     running = True
     cx, cy = TUK_WIDTH // 2, TUK_HEIGHT // 2
     frame = 0
-    sx,sy=cx,cy
+    set_new_target_arrow()
+    action = 3
+
+
+def set_new_target_arrow():
+    global sx, sy, hx, hy, t
+    sx, sy = cx, cy
     # hx, hy = 50, 50
-    hx, hy = random.randint(0,TUK_WIDTH), random.randint(0,TUK_HEIGHT) # p2:끝점
-    t= 0.0
-    action=3
+    hx, hy = random.randint(0, TUK_WIDTH), random.randint(0, TUK_HEIGHT)  # p2:끝점
+    t = 0.0
+
 
 def render_world():
     clear_canvas()
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-    arrow.draw(hx , hy)
+    arrow.draw(hx, hy)
     character.clip_draw(frame * 100, 100 * action, 100, 100, cx, cy)
     update_canvas()
+
 
 def update_world():
     global frame, cx, cy, t, action
     frame = (frame + 1) % 8
     action = 1 if cx < hx else 0
-    
-    if t<=1.0:
-        cx=(1-t)*sx+t*hx
-        cy=(1-t)*sy+t*hy
-        t+=0.001
-    
+    if t <= 1.0:
+        cx = (1 - t) * sx + t * hx
+        cy = (1 - t) * sy + t * hy
+        t += 0.001
+    else:
+        cx, cy = hx, hy
+        set_new_target_arrow()
 
 
 open_canvas(TUK_WIDTH, TUK_HEIGHT)
@@ -58,9 +67,9 @@ reset_world()
 
 
 while running:
-    render_world() # 월드의 현재 내용을 그린다.
-    handle_events() # 사용자 입력을 받아들인다.
-    update_world() # 월드 안의 객체들의 상호작용을 계산하고 그 결과를 update 한다.
-    
+    render_world()  # 월드의 현재 내용을 그린다.
+    handle_events()  # 사용자 입력을 받아들인다.
+    update_world()  # 월드 안의 객체들의 상호작용을 계산하고 그 결과를 update 한다.
+
 
 close_canvas()
